0b2a0ebcd73edb1cb9752c9b43904a29dc16f843
majestice_coder <142192726+Kapil9711@users.noreply.github.com>
2024-05-23 13:15:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
 //your JS code here. If required.
+const output = document.querySelector('#output');
+
+const defaultOutput = ()=> output.innerHTML = `<tr ><td colspan='2' >loading...</td> </tr>`;
+
+defaultOutput();
+
+const promise1 = new Promise((resolve,reject)=>{
+	const time = Math.floor(Math.random() * 3) + 1;
+	setTimeout(()=>{
+		resolve(time);
+	},time*1000);
+	
+})
+const promise2 = new Promise((resolve,reject)=>{
+	const time = Math.floor(Math.random() * 3) + 1;
+	setTimeout(()=>{
+		resolve(time);
+	},time*1000);
+	
+})
+const promise3 = new Promise((resolve,reject)=>{
+	const time = Math.floor(Math.random() * 3) + 1;
+	setTimeout(()=>{
+		resolve(time);
+	},time*1000);
+	
+})
+
+Promise.all([promise1,promise2,promise3]).then(values=>{
+	let tableBody = '';
+	values.forEach((value,i) =>{
+		tableBody += `<tr><td>Promise ${i+1}</td><td>${value} </td></tr>`;
+	})
+	tableBody += `<tr><td>Total</td><td>${values.reduce((acc,cur)=> acc+cur)} </td></tr>`;
+
+	
+	output.innerHTML = tableBody;	
+})
+
+
+
+
